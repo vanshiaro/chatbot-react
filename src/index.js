48e92620f0ed5afc84eb7d78843c55ff84eb8d7a
Vanshika Arora <you@example.comvanshiaro@gmail.com>
2022-05-18 00:38:20 +0530

    Index.js change

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <Hello  />
+    
   </div>
 );
 
